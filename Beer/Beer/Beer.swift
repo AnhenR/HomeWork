848debc176320fc6023456be97886f66dcdbd629
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-05-23 12:05:54 +0300

    beer.1

---
+++ Beer/Beer/Beer.swift
@@ -0,0 +1,25 @@
+//
+//  classBeer.swift
+//  Beer
+//
+//  Created by user on 20.05.22.
+//
+
+import Foundation
+import UIKit
+
+class Beer {
+    var name: String
+    var price: Double
+    var country: String
+    var volume: Double
+    var jar: Double
+    
+    init(name: String, price: Double, country: String, volume: Double, jar: Double ){
+        self.name = name
+        self.country = country
+        self.price = price
+        self.volume = volume
+        self.jar = jar
+    }
+}
---
+++ Beer/Beer/BeerSaleManager.swift
@@ -0,0 +1,53 @@
+//
+//  SaleBeer.swift
+//  Beer
+//
+//  Created by user on 20.05.22.
+//
+
+import Foundation
+
+class BeerSaleManager {
+    static let sale = BeerSaleManager()
+    private var proceedsAmount: Double = 0
+    
+    private init(){ }
+    
+    private var beer = [Beer(name: "Ipa", price: 3, country: "USA", volume: 1, jar: 0.3),
+                        Beer(name: "Lager", price: 4, country: "USA", volume: 40, jar: 0.4),
+                        Beer(name: "Stout", price: 5, country: "USA", volume: 50, jar: 0.5)]
+    
+    func updateProceedsAmount(_ name: String)  {
+        if let beer = beer.first(where: { $0.name == name }) {
+            if beer.volume - beer.jar >= 0 {
+                proceedsAmount += beer.price
+            }
+        }
+    }
+    
+    func updateBeerVolume(_ name: String) {
+        if let beer =  beer.first(where: { $0.name == name }) {
+            if beer.volume - beer.jar >= 0 {
+                beer.volume -= beer.jar
+            }
+        }
+    }
+    
+    func getVolume(_ name: String) -> Double {
+        if let beer = beer.first(where: { $0.name == name }) {
+            return beer.volume
+        }
+        else {
+            return 0
+        }
+    }
+    
+    func zeroProceedsAmount() -> Double {
+        proceedsAmount = 0
+        return proceedsAmount
+    }
+    
+    func getProceedsAmount() -> Double {
+        return proceedsAmount
+    }
+}
