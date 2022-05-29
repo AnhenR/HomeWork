8afb09c93b48c3ceec40e7c7798db90aaa6b48f6
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-05-29 16:12:13 +0300

    Add files via upload

---
+++ Game for cats/Game for cats/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Game for cats
+//
+//  Created by user on 27.05.22.
+//
+
+import UIKit
+let game = UIView()
+class ViewController: UIViewController {
+    func randomColor() -> UIColor{
+        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        game.backgroundColor = .red
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        game.frame.size = .init(width: 100, height: 100)
+        game.center = .init(x: view.frame.midX, y: view.frame.midY)
+        game.layer.cornerRadius = game.frame.height/2
+        view.addSubview(game)
+        game.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
+    }
+    @objc func didTap(){
+        game.frame.origin = .init(x: .random(in: 0...view.frame.maxX - game.frame.width), y: .random(in: 0...view.frame.maxY - game.frame.height))
+        game.backgroundColor = randomColor()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("1\(game.frame.origin)")
+        print("2 \(game.bounds.origin)")
+    }
+}
+
