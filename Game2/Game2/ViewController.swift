ff29f6a27d71c05bd385f58fc0c6643609f80e1a
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-05-30 15:13:54 +0300

    Add files via upload

--- Game2/Game2/ViewController.swift
+++ Game2/Game2/ViewController.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-let tapView = UIView()
 class ViewController: UIViewController {
+    let tapView = UIView()
     func randomColor() -> UIColor{
         return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
     }
@@ -23,16 +23,23 @@ class ViewController: UIViewController {
         tapView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
     }
     @objc func didTap(){
-        for x in 0...Int(view.frame.maxX) {
-            for y in 0...Int(view.frame.maxY){
-                var sq = UIView()
-                sq.frame.size = .init(width: 1, height: 1)
-                sq.frame.origin = .init(x: x, y: y)
-                sq.backgroundColor = randomColor()
-                view.addSubview(sq)
-            }
-        }
+//        for x in 0...Int(view.frame.maxX/10) {
+//            for y in 0...Int(view.frame.maxY/10) {
+//                var sq = UIView()
+//                sq.frame.size = .init(width: 10, height: 10)
+//                sq.frame.origin = .init(x: x * 10, y: y * 10)
+//                sq.backgroundColor = randomColor()
+//                view.addSubview(sq)
+//            }
+//        }
         
+        for x in 0...Int((view.frame.maxY * view.frame.maxX)/10) {
+            var sq1 = UIView()
+            sq1.backgroundColor = randomColor()
+            sq1.frame.size = .init(width: 10, height: 10)
+            sq1.frame.origin = .init(x: Int((x / Int(view.frame.maxX))*10) , y: Int((x % Int(view.frame.maxX))*10))
+            view.addSubview(sq1)
+        }
     }
 }
 
