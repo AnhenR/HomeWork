a60e59c4761f61ebbd2d0c024c1f4d3b350bdb88
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-05-30 23:11:25 +0300

    Add files via upload

--- Game3/Game3/ViewController.swift
+++ Game3/Game3/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
-let myCircle = UIView()
+private let myCircle = UIView()
+
+enum DirectionOfTravel {
+    case top
+    case bottom
+    case right
+    case left
+}
+
 class ViewController: UIViewController {
+    
     @IBOutlet weak var buttonTop: UIButton!
     @IBOutlet weak var buttonBottom: UIButton!
     @IBOutlet weak var buttonRight: UIButton!
     @IBOutlet weak var buttonLeft: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         buttonTop.layer.cornerRadius = 10
@@ -20,6 +30,7 @@ class ViewController: UIViewController {
         buttonLeft.layer.cornerRadius = 10
         myCircle.backgroundColor = .red
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         myCircle.frame.size = .init(width: 40, height: 40)
@@ -27,25 +38,41 @@ class ViewController: UIViewController {
         myCircle.layer.cornerRadius = myCircle.frame.height/2
         view.addSubview(myCircle)
     }
+    
     @IBAction func buttonTopTap(_ sender: Any) {
-        if myCircle.frame.origin.y > 0 {
-            myCircle.frame.origin.y -= 10
-        }
+        choiceOfDirection(direction: .top)
     }
+    
     @IBAction func buttonBottonTap(_ sender: Any) {
-        if myCircle.frame.origin.y < buttonTop.frame.origin.y - 50 {
-            myCircle.frame.origin.y += 10
-        }
+        choiceOfDirection(direction: .bottom)
     }
+    
     @IBAction func buttonRightTap(_ sender: Any) {
-        if myCircle.frame.origin.x < view.frame.maxX - myCircle.frame.width{
-            myCircle.frame.origin.x += 10
-        }
-        
+        choiceOfDirection(direction: .right)
     }
+    
     @IBAction func buttonLeftTap(_ sender: Any) {
-        if myCircle.frame.origin.x > 0 {
-            myCircle.frame.origin.x -= 10
+        choiceOfDirection(direction: .left)
+    }
+    
+    func choiceOfDirection(direction: DirectionOfTravel){
+        switch direction {
+        case .top:
+            if myCircle.frame.origin.y > 0 {
+                myCircle.frame.origin.y -= 10
+            }
+        case .bottom:
+            if myCircle.frame.origin.y < buttonTop.frame.origin.y - 50 {
+                myCircle.frame.origin.y += 10
+            }
+        case .right:
+            if myCircle.frame.origin.x < view.frame.maxX - myCircle.frame.width{
+                myCircle.frame.origin.x += 10
+            }
+        case .left:
+            if myCircle.frame.origin.x > 0 {
+                myCircle.frame.origin.x -= 10
+            }
         }
     }
 }
