1e53fa529e7a4f6f91c52e761fd4761a7a63a15f
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-06-08 12:02:19 +0300

    Добавлена игра с Tap

---
+++ Game for cats/Game for cats/GameTap.swift
@@ -0,0 +1,56 @@
+//
+//  GameTap.swift
+//  Game for cats
+//
+//  Created by user on 8.06.22.
+//
+
+import Foundation
+import UIKit
+
+private let ball = UIView()
+//private let singleTapGestureRecognizerView = UITapGestureRecognizer()
+//private let singleTapGestureRecognizerBall = UITapGestureRecognizer()
+//private let doubleTapGestureRecognizerBall = UITapGestureRecognizer()
+
+class GameTap: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        ball.backgroundColor = .green
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ball.frame.size = .init(width: 60, height: 60)
+        ball.layer.cornerRadius = ball.frame.height/2
+        ball.center = .init(x: view.frame.midX, y: view.frame.midY)
+        view.addSubview(ball)
+        let singleTapGestureRecognizerView = UITapGestureRecognizer(target: self, action:#selector(handleSingleTapGestureView))
+        view.addGestureRecognizer(singleTapGestureRecognizerView)
+        let singleTapGestureRecognizerBall = UITapGestureRecognizer(target: self, action:#selector(handleTapGestureBall))
+        singleTapGestureRecognizerBall.numberOfTapsRequired = 1
+        ball.addGestureRecognizer(singleTapGestureRecognizerBall)
+        let doubleTapGestureRecognizerBall = UITapGestureRecognizer(target: self, action:#selector(handleTapGestureBall))
+        doubleTapGestureRecognizerBall.numberOfTapsRequired = 2
+        ball.addGestureRecognizer(doubleTapGestureRecognizerBall)
+        
+    }
+    
+    @objc func handleSingleTapGestureView(tapView: UITapGestureRecognizer) {
+        UIView.animate(withDuration: 1.0){
+            ball.frame = CGRect(x: tapView.location(in: self.view).x - ball.frame.size.height / 2, y: tapView.location(in: self.view).y - ball.frame.size.width / 2 , width: ball.frame.size.width, height:ball.frame.size.height)
+            ball.isHidden = false
+            self.view.addSubview(ball)
+        }
+    }
+    @objc func handleTapGestureBall(tapBall: UITapGestureRecognizer) {
+            if tapBall.numberOfTapsRequired == 2 {
+                ball.isHidden = true
+            }
+            else if tapBall.numberOfTapsRequired == 1 {
+                ball.backgroundColor = .red
+            }
+    }
+}
--- Game for cats/Game for cats/ViewController.swift
+++ Game for cats/Game for cats/ViewController.swift
@@ -10,19 +10,31 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var catLable: UILabel!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var ballStartButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.layer.cornerRadius = 20
+        ballStartButton.layer.cornerRadius = 20
     }
     
     @IBAction func startButtonAction(_ sender: Any) {
         tapGameCat()
     }
     
+    @IBAction func ballStartButtonAction(_ sender: Any) {
+        tapBallGame()
+    }
+  
     private func tapGameCat(){
         let storyboard = UIStoryboard(name: "GameCatStoryboard", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "GameCat")
+        viewController.modalPresentationStyle = .fullScreen
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    private func tapBallGame(){
+        let storyboard = UIStoryboard(name: "GameTapStoryboard", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "GameTap")
         viewController.modalPresentationStyle = .fullScreen
         navigationController?.pushViewController(viewController, animated: true)
     }
