ee2878f58ca3e22b57a87eea69c0ff66bccf3294
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-06-15 04:15:29 +0300

    Исправлены квадраты

--- StackSquares/StackSquares/ViewController.swift
+++ StackSquares/StackSquares/ViewController.swift
@@ -8,42 +8,186 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var rootStackView: UIStackView!
-    @IBOutlet weak var firstView: UIView!
-    @IBOutlet weak var tapButton: UIButton!
-    @IBOutlet weak var tapAppearButton: UIButton!
+    
+    private lazy var firstView = makeFirstView()
+    private lazy var secondView = makeSecondView()
+    private lazy var thirdView = makeThirdView()
+    private lazy var fourthView = makeFourthView()
+    
+    private var firstTopAnchor: NSLayoutConstraint?
+    private var firstBottomAnchor: NSLayoutConstraint?
+    private var firstleadingAnchor: NSLayoutConstraint?
+    private var firstTrailingAnchor: NSLayoutConstraint?
+    
+    private var secondTopAnchor: NSLayoutConstraint?
+    private var secondBottomAnchor: NSLayoutConstraint?
+    private var secondLeadingAnchor: NSLayoutConstraint?
+    private var secondTrailingAnchor: NSLayoutConstraint?
+    
+    private var thirdTopAnchor: NSLayoutConstraint?
+    private var thirdBottomAnchor: NSLayoutConstraint?
+    private var thirdLeadingAnchor: NSLayoutConstraint?
+    private var thirdTrailingAnchor: NSLayoutConstraint?
+    
+    private var fourthTopAnchor: NSLayoutConstraint?
+    private var fourthBottomAnchor: NSLayoutConstraint?
+    private var fourthLeadingAnchor: NSLayoutConstraint?
+    private var fourthTrailingAnchor: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tapButton.layer.cornerRadius = 15
-        tapAppearButton.layer.cornerRadius = 15
+        setupFirstView()
+        setupSecondView()
+        setupThirdView()
+        setupFourthView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        rootStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
-        firstView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapView)))
+        let singleTapFirstView = UITapGestureRecognizer(target: self, action:#selector(handleSingleTapFirstView))
+        firstView.addGestureRecognizer(singleTapFirstView)
+        let singleTapSecondView = UITapGestureRecognizer(target: self, action:#selector(handleSingleTapSecondView))
+        secondView.addGestureRecognizer(singleTapSecondView)
+        let singleTapThirdView = UITapGestureRecognizer(target: self, action:#selector(handleSingleTapThirdView))
+        thirdView.addGestureRecognizer(singleTapThirdView)
+        let singleTapFourthView = UITapGestureRecognizer(target: self, action:#selector(handleSingleTapFourthView))
+        fourthView.addGestureRecognizer(singleTapFourthView)
+        
+        let upGestureFirst = UISwipeGestureRecognizer(target: self, action: #selector(didSwipeFirstView))
+        upGestureFirst.direction = .up
+        firstView.addGestureRecognizer(upGestureFirst)
+        let downGestureThird = UISwipeGestureRecognizer(target: self, action: #selector(didSwipeThirdView))
+        downGestureThird.direction = .down
+        thirdView.addGestureRecognizer(downGestureThird)
+        
+        let panSecond = UIPanGestureRecognizer(target: self, action: #selector(handlePanSecond))
+        secondView.addGestureRecognizer(panSecond)
+        let panFourth = UIPanGestureRecognizer(target: self, action: #selector(handlePanFourth))
+        fourthView.addGestureRecognizer(panFourth)
+        
         
     }
     
-    @objc func didTap(){
-        rootStackView.frame.origin = .init(x: .random(in: 0...view.frame.maxX - rootStackView.frame.width), y: .random(in: 0...view.frame.maxY - rootStackView.frame.height))
+    private func makeFirstView() -> UIView {
+        var myView = UIView()
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.backgroundColor = .blue
+        return myView
     }
     
-    @objc func didTapView(){
-        firstView.frame =  .init(x: firstView.frame.origin.x, y: firstView.frame.origin.y, width: 10, height: 10)
-    }
-
-
-    
-    @IBAction func tapButtonAction(_ sender: Any) {
-        rootStackView.isHidden = true
+    private func setupFirstView() {
+        view.addSubview(firstView)
+        firstTopAnchor = firstView.topAnchor.constraint(equalTo: view.topAnchor, constant: 338)
+        firstleadingAnchor = firstView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 97)
+        firstTrailingAnchor = firstView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -217)
+        firstBottomAnchor = firstView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -458)
+        firstTopAnchor?.isActive = true
+        firstleadingAnchor?.isActive = true
+        firstTrailingAnchor?.isActive = true
+        firstBottomAnchor?.isActive = true
         
     }
     
-    @IBAction func tapAppearButtonAction(_ sender: Any) {
-        rootStackView.isHidden = false
+    private func makeSecondView() -> UIView {
+        var myView = UIView()
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.backgroundColor = .cyan
+        return myView
+    }
+    
+    private func setupSecondView() {
+        view.addSubview(secondView)
+        secondTopAnchor = secondView.topAnchor.constraint(equalTo: view.topAnchor, constant: 338)
+        secondLeadingAnchor = secondView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 217)
+        secondTrailingAnchor = secondView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -97)
+        secondBottomAnchor = secondView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -458)
+        secondTopAnchor?.isActive = true
+        secondLeadingAnchor?.isActive = true
+        secondTrailingAnchor?.isActive = true
+        secondBottomAnchor?.isActive = true
+    }
+    
+    private func makeThirdView() -> UIView {
+        var myView = UIView()
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.backgroundColor = .darkGray
+        return myView
+    }
+    
+    private func setupThirdView() {
+        view.addSubview(thirdView)
+        thirdTopAnchor = thirdView.topAnchor.constraint(equalTo: view.topAnchor, constant: 458)
+        thirdLeadingAnchor = thirdView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 97)
+        thirdTrailingAnchor = thirdView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -217)
+        thirdBottomAnchor = thirdView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -338)
+        thirdTopAnchor?.isActive = true
+        thirdLeadingAnchor?.isActive = true
+        thirdTrailingAnchor?.isActive = true
+        thirdBottomAnchor?.isActive = true
+    }
+    
+    private func makeFourthView() -> UIView {
+        var myView = UIView()
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.backgroundColor = .magenta
+        return myView
+    }
+    
+    private func setupFourthView() {
+        view.addSubview(fourthView)
+        fourthTopAnchor = fourthView.topAnchor.constraint(equalTo: view.topAnchor, constant: 458)
+        fourthLeadingAnchor = fourthView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 217)
+        fourthTrailingAnchor = fourthView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -97)
+        fourthBottomAnchor = fourthView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -338)
+        fourthTopAnchor?.isActive = true
+        fourthLeadingAnchor?.isActive = true
+        fourthTrailingAnchor?.isActive = true
+        fourthBottomAnchor?.isActive = true
+    }
+    
+    @objc func handleSingleTapFirstView(tapView: UITapGestureRecognizer) {
+        firstView.isHidden = true
+    }
+    
+    @objc func handleSingleTapSecondView(tapView: UITapGestureRecognizer) {
+        secondView.isHidden = true
+    }
+    
+    @objc func handleSingleTapThirdView(tapView: UITapGestureRecognizer) {
+        thirdView.isHidden = true
+    }
+    
+    @objc func handleSingleTapFourthView(tapView: UITapGestureRecognizer) {
+        fourthView.isHidden = true
+    }
+    
+    @objc func didSwipeFirstView(sender: UISwipeGestureRecognizer){
+        firstTopAnchor?.isActive = false
+        firstleadingAnchor?.isActive = false
+        if sender.direction == .up {
+            self.firstTopAnchor?.constant = 328
+            self.firstleadingAnchor?.constant = 87
+            firstTopAnchor?.isActive = true
+            firstleadingAnchor?.isActive = true
+        }
+    }
+    
+    @objc func didSwipeThirdView(sender: UISwipeGestureRecognizer){
+        thirdLeadingAnchor?.isActive = false
+        thirdBottomAnchor?.isActive = false
+        if sender.direction == .down {
+            self.thirdBottomAnchor?.constant = -328
+            self.thirdLeadingAnchor?.constant = 87
+            thirdLeadingAnchor?.isActive = true
+            thirdBottomAnchor?.isActive = true
+        }
+    }
+    
+    @objc func handlePanSecond(panSecond: UIPanGestureRecognizer){
+        secondView.frame.origin = panSecond.location(in: view)
+    }
+    
+    @objc func handlePanFourth(panFourth: UIPanGestureRecognizer){
+        fourthView.frame.origin = panFourth.location(in: view)
     }
 }
-
