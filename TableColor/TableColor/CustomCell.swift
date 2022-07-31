fd6f8093311b1c3cd8f25ae606e8a331c6ad87b9
AnhenR <105051643+AnhenR@users.noreply.github.com>
2022-07-31 23:06:23 +0300

    Add files via upload

---
+++ TableColor/TableColor/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  TableColor
+//
+//  Created by user on 31.07.22.
+//
+
+import Foundation
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var colorButton: UIButton!
+    
+    @IBOutlet weak var colorButtonAction: UIButton!
+    
+}
